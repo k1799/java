f81285adab7ab99a114325e903b5eca65012bce9
k1799 <gkannan1799@gmail.com>
2026-04-06 19:27:32 +0530

    added java operators program

---
+++ operators.java
@@ -0,0 +1,24 @@
+class Operators
+{
+	public static void main(String[]args)
+	{
+		//Arithmetic operators
+		int a=20;
+		int b=60;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		//Relational(comparison) operators
+		int c=10;
+		int d=20;
+		System.out.println(c==d);
+		System.out.println(c!=d);
+		System.out.println(c>d);
+		System.out.println(c<d);
+		System.out.println(c>=d);
+		System.out.println(c<=d);
+		
+	}
+}
